020a24b79950052510f41480d33eb381e753cada
avshockeyw <avshockeyw@users.noreply.github.com>
2023-01-03 21:03:52 -0500

    debugging 2

--- keyboards/reviung/reviung41/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/reviung/reviung41/keymaps/manna-harbour_miryoku/config.h
@@ -5,17 +5,16 @@
 
 #pragma once
 
-#define LAYOUT_miryoku( \
-K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09, \
-K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19, \
-K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, \
-N30,   N31,   K32,   K33,   K34,          K35,   K36,   K37,   N38,   N39 \
+#define MIRYOKU_MAPPING( \
+     K40, K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, K44, \
+     K41, K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, K45, \
+     K42, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, K46, \
+     N43, N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39, N47 \
 ) \
-LAYOUT_split_3x5_2( \
-K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09, \
-K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19, \
-K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, \
-                     K33,   K34,          K35,   K36 \
+LAYOUT_reviung41( \
+K40,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  K44, \
+K41,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  K45, \
+K42,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  K46 , \
+                           K33,  K34,   K35,  K36,  K37 \
 )
-
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"

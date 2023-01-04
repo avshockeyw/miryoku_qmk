8d8e1d95476130a03d6f95ba90a4754698da4c03
avshockeyw <avshockeyw@users.noreply.github.com>
2023-01-04 08:21:28 -0500

    Update custom_rules.mk

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,10 +1,5 @@
-# Copyright 2019 Manna Harbour
-# https://github.com/manna-harbour/miryoku
+CONVERT_TO=elite_pi
 
-MIRYOKU_KLUDGE_THUMBCOMBOS=yes
-MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
-CONVERT_TO=elite_pi

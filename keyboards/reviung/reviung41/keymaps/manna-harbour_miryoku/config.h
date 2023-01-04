f95e7ba24b4a91d546baa34b09cb666a76edd54b
avshockeyw <avshockeyw@users.noreply.github.com>
2023-01-03 20:59:34 -0500

    debugging

--- keyboards/reviung/reviung41/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/reviung/reviung41/keymaps/manna-harbour_miryoku/config.h
@@ -18,21 +18,4 @@ K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29, \
                      K33,   K34,          K35,   K36 \
 )
 
-#ifdef RGBLIGHT_ENABLE
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_CHRISTMAS
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_RGB_TEST
-    #define RGBLIGHT_EFFECT_ALTERNATING
-    #define RGBLIGHT_EFFECT_TWINKLE
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
-
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
